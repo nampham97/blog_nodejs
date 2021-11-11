d1beff4e24cac717a416b0b01955129ec536cb3f
nampd <nampd@hpt.vn>
2021-11-11 15:05:27 +0700

    Update project

---
+++ src/index.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const morgan = require('morgan')
+const handlebars = require('express-handlebars')
+const path = require('path')
+const app = express()
+const port = 3000
+
+//Public path
+app.use(express.static(path.join(__dirname, 'public')))
+
+//HTTP Logger
+app.use(morgan('combined'))
+
+//Template Engine
+app.engine('hbs', handlebars({
+  extname : '.hbs'
+}))
+app.set('view engine', 'hbs')
+app.set('views', path.join(__dirname, 'resources/views'))
+app.get('/', (req, res) => {
+  res.render('home');
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
